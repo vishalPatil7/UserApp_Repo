fefbabc7b9d63a2db58f79c5bf1956f5d15a665b
Vishal Patil <vishal.i.patil@capgemini.com>
2024-12-17 12:17:33 +0530

    new changes

--- UserApp/Controllers/AccountController.cs
+++ UserApp/Controllers/AccountController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserApp.Models;
+using UserApp.ViewModels;
 
 namespace UserApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+
+        public AccountController(SignInManager<Users> signInManager, UserManager<Users> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or password is incorrect.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+
+                var result = await userManager.CreateAsync(users, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+
+                    return View(model);
+                }
+            }
+            return View(model);
         }
     }
 }
--- UserApp/ViewModels/LoginViewModel.cs
+++ UserApp/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace UserApp.ViewModels
 {
@@ -7,5 +8,12 @@ namespace UserApp.ViewModels
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
